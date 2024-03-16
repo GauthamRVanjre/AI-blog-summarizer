ded75fd48ca4e5d660606d4e2b8b81f1f74c0593
Gautham Vanjre <vanjregautham@gmail.com>
2024-03-16 20:59:51 +0530

    feat: UI for hero and demo section

--- src/components/Demo.tsx
+++ src/components/Demo.tsx
@@ -1,7 +1,58 @@
-import React from "react";
-
+import { useState } from "react";
+// import copy from "../assets/copy.svg";
+import linkIcon from "../assets/link.svg";
+// import grid from "../assets/grid.svg";
+// import loader from "../assets/loader.svg";
+// import tick from "../assets/tick.svg";
 const Demo = () => {
-  return <div>Demo</div>;
+  const [article, setArticle] = useState({
+    url: "",
+    summary: "",
+  });
+
+  const handleSubmit = async () => {
+    alert("Submit");
+  };
+
+  return (
+    <section className="mt-16 w-full max-w-xl">
+      {/* Search  */}
+      <div className="flex flex-col w-full gap-2">
+        <form
+          onSubmit={handleSubmit}
+          className="relative flex justify-center items-center"
+        >
+          <img
+            src={linkIcon}
+            alt="link_icon"
+            className="absolute left-0 my-2 ml-3 w-5"
+          />
+          <input
+            type="url"
+            placeholder="Enter a URL"
+            value={article.url}
+            onChange={(e) =>
+              setArticle({
+                ...article,
+                url: e.target.value,
+              })
+            }
+            required
+            className="url_input peer"
+          />
+          <button
+            type="submit"
+            className="submit_btn peer-focus:border-gray-700 peer-focus:text-gray-700"
+          >
+            /
+          </button>
+        </form>
+
+        {/* History of URL */}
+      </div>
+      {/* display results */}
+    </section>
+  );
 };
 
 export default Demo;
--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -1,7 +1,22 @@
-import React from "react";
-
+import logo from "../assets/logo.svg";
 const Hero = () => {
-  return <div>Hero</div>;
+  return (
+    <header className="w-full flex justify-center items-center flex-col">
+      <nav className="flex justify-between items-center w-full mb-10 pt-3">
+        <img src={logo} alt="logo" className="w-28 object-contain" />
+
+        <button type="button" className="black_btn">
+          GitHub
+        </button>
+      </nav>
+
+      <h1 className="head_text">
+        Summarize Articles with <br className="max-md:hidden" />
+        <span className="orange_gradient">OpenAI GPT-4</span>
+      </h1>
+      <h2 className="desc">Summarize your articles</h2>
+    </header>
+  );
 };
 
 export default Hero;
